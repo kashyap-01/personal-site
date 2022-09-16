67411e11b01c618df103395b4f5f797b8b2f5777
kashyap-01 <80740829+kashyap-01@users.noreply.github.com>
2022-09-16 14:26:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("space")
+favouriteMovieGenre("military")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
-favouriteFruit("blueberry")
+favouriteFruit("avocado")
 
 // light, dark
 favouriteMode("dark")
